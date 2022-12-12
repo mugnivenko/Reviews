a0430c1218a1b105f05153db59bfb7755482a68d
ugnivenko <matvey.ugnivenko@gmail.com>
2022-12-12 20:32:04 +0400

    [initial commit] generate project and add dbcontext

---
+++ Data/AplicationDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Reviews.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
